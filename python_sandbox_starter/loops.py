75dbef75bb850d7a484d76ddf72e46eb08800083
toriwat328 <toriwat328@gmail.com>
2020-02-17 11:00:56 -0500

    added examples of loops for and while in python as well as ranges continue and break

--- python_sandbox_starter/loops.py
+++ python_sandbox_starter/loops.py
@@ -23,14 +23,14 @@ people = ['John', 'Paul', 'Sara', 'Susan']
 #     print(people[i])
 
 #Custom range Not going to go to 11 it goes to the number before that
-for i in range(0, 11):
-    print(f'Number: {i}')
+# for i in range(0, 11):
+#     print(f'Number: {i}')
 
 
 # While loops execute a set of statements as long as a condition is true.
 
-count = 0
-
-while count <= 10:
-    print(f'Count: {count}')
-    count +=1
+# count = 0
+#
+# while count <= 10:
+#     print(f'Count: {count}')
+#     count +=1
