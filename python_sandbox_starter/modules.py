c403601145b30873e67d4b97c57956def90b9f2e
toriwat328 <toriwat328@gmail.com>
2020-02-17 11:23:29 -0500

    understanding of importing modules with set functions that can be used in your program core, pip, custom modules

--- python_sandbox_starter/modules.py
+++ python_sandbox_starter/modules.py
@@ -1,1 +1,36 @@
 # A module is basically a file containing a set of functions to include in your application. There are core python modules, modules you can install using the pip package manager (including Django) as well as custom modules
+
+# Core Modules
+import datetime
+from datetime import date
+from time import time
+
+# Pip module
+# Makes every first char in word uppercase
+from camelcase import CamelCase
+
+# Import custom module
+from validator import validate_email
+
+
+today = date.today()
+
+# today = datetime.date.today()
+
+#current timestamp
+timestamp = time()
+
+c = CamelCase()
+
+# print(c.hump('hello there world'))
+
+email = 'test@test.com'
+
+if validate_email(email):
+    print('Email is valid')
+else:
+    print('Email is bad')
+
+
+# print(today)
+# print(timestamp)
